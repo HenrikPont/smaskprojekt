cb0654924a8fdff2d9cde9329b3227e5f0aeb582
elinmatsson26 <elin.matsson@telia.com>
2026-02-20 14:21:56 +0100

    Ännu bättre qda

--- qda1.py
+++ qda1.py
@@ -6,7 +6,7 @@ import sklearn.preprocessing as skl_pre
 import sklearn.linear_model as skl_lm
 import sklearn.discriminant_analysis as skl_da
 import sklearn.neighbors as skl_nb
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, f1_score
 from sklearn.model_selection import GridSearchCV
 
 import features 
@@ -42,8 +42,24 @@ print(f"Bästa parametrar: {grid.best_params_}")
 print(f"Bästa CV F1-score: {grid.best_score_:.4f}")
 best_model = grid.best_estimator_
 
+
+probabilities = best_model.predict_proba(X_test)[:, 1]
+thresholds = np.arange(0.1, 0.9, 0.01)
+f1_scores = []
+
+for threshold in thresholds:
+    predictions = (probabilities >= threshold).astype(int)
+    f1 = f1_score(y_test, predictions)
+    f1_scores.append(f1)
+
+best_idx = np.argmax(f1_scores)
+best_threshold = thresholds[best_idx]
+print(f"Bästa tröskel: {best_threshold:.2f} med F1-score: {f1_scores[best_idx]:.4f}")
+
+
 #Det man kom fram till
-prediction = best_model.predict(X_test)
+#prediction = best_model.predict(X_test)
+prediction = (probabilities >= best_threshold).astype(int)
 
 print("Confusion Matrix:")
 print(pd.crosstab(prediction, y_test, rownames=['Predicted'], colnames=['Actual']))
