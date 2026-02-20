b49d8930b84560dcf1ebaa60118609cf7f6001b6
elinmatsson26 <elin.matsson@telia.com>
2026-02-20 08:47:36 +0100

    qda grej

--- qda1.py
+++ qda1.py
@@ -9,10 +9,10 @@ import sklearn.neighbors as skl_nb
 from sklearn import model_selection as ms
 from sklearn.metrics import classification_report
 
-from features import combined_features
+import features 
 
 #Hämta faeatures
-df = combined_features()
+bikes = features.combined_features()
 
 selected_features = [
     'weather_pca1', 'weather_pca2', 
@@ -24,11 +24,23 @@ selected_features = [
     'windspeed',
 ]
 
-X = df[selected_features]
-y = df['increase_stock']
+X = bikes[selected_features]
+y = bikes['increase_stock']
+
+np.random.seed(0)
+
+trainI = np.random.choice(bikes.shape[0], size=800, replace=False)
+trainIndex = bikes.index.isin(trainI)
+train = bikes.iloc[trainIndex]
+test = bikes.iloc[~trainIndex]
+
+X_train = train[selected_features]
+y_train = np.where(train['increase_stock'] == 'high_bike_demand', 1, 0)
+X_test = test[selected_features]
+y_test = np.where(test['increase_stock'] == 'high_bike_demand', 1, 0)
 
 # 3. Splitta datan
-X_train, X_test, y_train, y_test = ms.train_test_split(X, y, test_size=0.2, random_state=42)
+#X_train, X_test, y_train, y_test = ms.train_test_split(X, y, test_size=0.2, random_state=42)
 
 # 4. Skapa och träna QDA
 # Tips: Vi kan manuellt sätta "priors" om vi vill tvinga modellen 
